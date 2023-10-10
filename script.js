cd343739578e45b5ee333160244f5cd152a6f177
Asma Noor <asma@Asmas-MacBook-Air.local>
2023-10-10 11:02:55 -0500

    Solution to 2.1.10

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
-    $("th").css({"font-size": "20px","font-style": "italic"
-    });
-    $('tbody tr td:nth-child(odd)').css({
-        "background-color":"lightblue"
-    })
-    $('tbody tr td:nth-child(even)').css({
-        "background-color": "tomato"
-    })
+    $("th").css({"font-size": "20px","font-style": "italic"});
+    $('tbody tr td:nth-child(odd)').css({"background-color":"lightblue" })
+
+
+    checkCost();
+
+        function checkCost(){
+            for(var i=1; i<=$("tr").length; i++){
+                var item = $("tr:nth-child("+ i +")td:nth-child(2)");
+                console.log("item");
+                var cost = parseFloat(item.text());
+                console.log("cost");
+
+                if (cost > 10){
+                    item.css("background-color", "tomato");
+                }
+
+
+            }
+        }
+
 });
+
+
